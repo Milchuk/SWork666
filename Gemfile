9b78b12d8d2ded680f69e01c78bcf657cad78138
Milchuk <genii9989@gmail.com>
2020-02-16 19:51:00 +0300

    Commit before registration dev

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ end
 gem 'rails', '~> 5.1.6', '>= 5.1.6.1'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
+
+gem 'carrierwave'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
+  root to: redirect('/ideas')
+  resources :ideas
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
